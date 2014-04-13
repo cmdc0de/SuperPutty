5e76fe0d49724dfab0135c7988e5104bad8e906f
Sylvain MOUQUET <sylvain.mouquet@gmail.com>
2014-04-13 14:31:31 +0200

    ajout native treeview

---
+++ SuperPutty/Utils/NativeTreeView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SuperPutty.Utils
+{
+    public class NativeTreeView : System.Windows.Forms.TreeView
+    {
+        [DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
+        private extern static int SetWindowTheme(IntPtr hWnd, string pszSubAppName,
+                                                string pszSubIdList);
+
+        protected override void CreateHandle()
+        {
+            base.CreateHandle();
+
+            SetWindowTheme(this.Handle, "explorer", null);
+        }
+    }
+}
